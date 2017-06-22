2b05551d50a71477eeb7075c17f1d3a9aa5a8593
Lena Ripple <ripplelena@gmail.com>
2017-06-22 16:00:31 -0600

    syntax fixed

--- app/knexfile.js
+++ app/knexfile.js
@@ -19,7 +19,7 @@ module.exports = {
   production: {
     client:"pg",
     connection:{
-      database: process.env.DATABASE_URL + '?ssl=true'
+      database: process.env.DATABASE_URL + '?ssl=true',
     }
   }
 };
